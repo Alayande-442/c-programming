0830d190d9b75fdba8ff45c281c98affbcc80c81
Alayande olaoluwa Stephen <alayandesteven@gmail.com>
2023-07-02 22:45:16 +0100

    introduction to c programming

---
+++ app.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// int main() {
+
+//     // printf("Hello World\n");
+//     // printf("how are you doing today");
+//     // printf("    /|\n");
+//     // printf("   / |\n");
+//     // printf("  /  |\n");
+//     // printf(" /   |\n");
+//     // printf("/____|\n");
+
+//     // variable in c programming
+//     // char characterName[] = "olaoluwa";
+//     // int characterAge = 35;
+//     // char nameOfProgrammer[] = "Aruwajoye pelumi";
+//     // int numberOfValue = 20;
+
+//     // printf("my name is %s \n", characterName);
+//     // printf("i'm %d years old \n", characterAge);
+
+//     // characterAge = 60;
+//     // printf("you know sometimes we live to %d years old", characterAge);
+//     // printf("%s is the leading press director of tvc news with over %d years of experience \n", nameOfProgrammer, numberOfValue);
+
+//     // data type in c programming
+//     // double doublename = 3.63;
+
+//     // char cgpa = "A";
+//     // char letterName[] = "letter name";
+
+//     // printf("my name is %s and my CGPA is %f \n",letterName, doublename); 
+//     // printf("my favorite number is %d and i love %s ", 500, "olaoluwa");
+
+//     // ******WORKING WITH NUMBERS IN C PROGRAMMING ******//
+
+//     // printf("%f", 3.5 * 4.5 / 2.3);
+//     // printf("%d", 200 + 300 - 100);
+//     // printf("%f", 10 + 4.5);
+
+//     // printf("%f", pow(2, 3));
+//     // printf("%f", sqrt(36));
+//     //printf("%f", ceil(36.8));
+//     // printf("%f", floor(39.99));
+
+//     /* commenting in c programming */
+//     /*
+//     whfjdkjvkfcdkvkv kvkcfvkvkvkvkfckvkfc
+//     */
+
+//    /* CONSTANTS IN C PROGRAMMING */
+//     return 0;
+// }
+
+///// ******* constant in C PROGRAMMING ********////// 
+
+int main() {
+
+   printf("%d", 500);
+   
+    return 0;
+} 
