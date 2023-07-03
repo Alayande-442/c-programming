80a404a41cf9ab8d2f8d0f261ae3c3d3f293ae98
Alayande olaoluwa Stephen <alayandesteven@gmail.com>
2023-07-03 22:02:31 +0100

    variable in c programming

--- app.c
+++ app.c
@@ -1,63 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// int main() {
+ int main() {
+    printf("Hello world!\n");
+    printf("   /|\n");
+    printf("  / |\n");
+    printf(" /  |\n");
+    printf("/___|\n");
 
-//     // printf("Hello World\n");
-//     // printf("how are you doing today");
-//     // printf("    /|\n");
-//     // printf("   / |\n");
-//     // printf("  /  |\n");
-//     // printf(" /   |\n");
-//     // printf("/____|\n");
-
-//     // variable in c programming
-//     // char characterName[] = "olaoluwa";
-//     // int characterAge = 35;
-//     // char nameOfProgrammer[] = "Aruwajoye pelumi";
-//     // int numberOfValue = 20;
-
-//     // printf("my name is %s \n", characterName);
-//     // printf("i'm %d years old \n", characterAge);
-
-//     // characterAge = 60;
-//     // printf("you know sometimes we live to %d years old", characterAge);
-//     // printf("%s is the leading press director of tvc news with over %d years of experience \n", nameOfProgrammer, numberOfValue);
-
-//     // data type in c programming
-//     // double doublename = 3.63;
-
-//     // char cgpa = "A";
-//     // char letterName[] = "letter name";
-
-//     // printf("my name is %s and my CGPA is %f \n",letterName, doublename); 
-//     // printf("my favorite number is %d and i love %s ", 500, "olaoluwa");
-
-//     // ******WORKING WITH NUMBERS IN C PROGRAMMING ******//
-
-//     // printf("%f", 3.5 * 4.5 / 2.3);
-//     // printf("%d", 200 + 300 - 100);
-//     // printf("%f", 10 + 4.5);
-
-//     // printf("%f", pow(2, 3));
-//     // printf("%f", sqrt(36));
-//     //printf("%f", ceil(36.8));
-//     // printf("%f", floor(39.99));
-
-//     /* commenting in c programming */
-//     /*
-//     whfjdkjvkfcdkvkv kvkcfvkvkvkvkfckvkfc
-//     */
-
-//    /* CONSTANTS IN C PROGRAMMING */
-//     return 0;
-// }
-
-///// ******* constant in C PROGRAMMING ********////// 
-
-int main() {
-
-   printf("%d", 500);
    
+   char characterName[] = "olaoluwa";
+   int ageName = 35; 
+   printf("There once was a man named %s\n", characterName);
+   printf("he's %d years old.\n", ageName);
+   printf("he really likes the name %s\n", characterName);
+   printf("but did no like being %d", ageName);
     return 0;
-} 
+ }
