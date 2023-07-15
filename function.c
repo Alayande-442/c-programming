5d0ee722033af149711a0d44aed5f972ce366c69
Alayande olaoluwa Stephen <alayandesteven@gmail.com>
2023-07-15 15:41:50 +0100

    return statement in c

--- function.c
+++ function.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int powerOfNumber(int num) {
+    int result = num * num * num;
+    return result;
+
+}
+
+double bodmas(double number, double num2, double num3) {
+    double cal = number * number / num2 + num3;
+    return cal;
+}
 int main() {
 
-    sayHi("olaoluwa");
-    sayHi("odel");
-    sayHi("kunle");
+    sayHi("olaoluwa", 40, "yabatech");
+    sayHi("odel", 30, "babcock");
+    sayHi("kunle", 60, "unilag");
+
+    // *********** RETURN STATEMENT IN C************
+    printf("answer:%d", powerOfNumber(3));
+    printf ("answer:%f", bodmas(5,2,2));
+
     return 0;
 }
 
-void sayHi(char name[]) {
-    printf("hello %s\n", name);
+void sayHi(char name[], int age, char school[]) {
+    printf("hello %s you are %d years old and you are student of %s\n", name, age, school);
 }
