a6f83a355e7da7de94f378cb287bb364a687a4c6
Alayande olaoluwa Stephen <alayandesteven@gmail.com>
2023-07-15 10:49:55 +0100

    variable in c

--- app.c
+++ app.c
@@ -2,18 +2,26 @@
 #include<stdlib.h>
 
  int main() {
-    printf("Hello world!\n");
-    printf("   /|\n");
-    printf("  / |\n");
-    printf(" /  |\n");
-    printf("/___|\n");
-
+   //  printf("Hello world!\n");
+   //  printf("   /|\n");
+   //  printf("  / |\n");
+   //  printf(" /  |\n");
+   //  printf("/___|\n");
    
-   char characterName[] = "olaoluwa";
-   int ageName = 35; 
-   printf("There once was a man named %s\n", characterName);
-   printf("he's %d years old.\n", ageName);
-   printf("he really likes the name %s\n", characterName);
-   printf("but did no like being %d", ageName);
+   // char characterName[] = "olaoluwa";
+   // int ageName = 35; 
+   // printf("There once was a man named %s\n", characterName);
+   // printf("he's %d years old.\n", ageName);
+   // printf("he really likes the name %s\n", characterName);
+   // printf("but did no like being %d\n", ageName);
+   // printf("hello world\n");
+   // printf("%d\n", 500);
+   // printf("my fav %s is %d\n", "number", 400);
+   // printf("%f\n",500 * 2.0);
+   // printf("%f\n", 5 / 2.0);
+   // printf("%f\n", pow(3,3) );
+   // printf("%f\n", pow(2,3));
+   // printf("%f\n", ceil(36.544));
+   // printf("%f\n", floor(39.677));
     return 0;
  }
---
+++ app2.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int num = 5;
+    printf("%d\n", num);
+    num = 8;
+    printf("%d\n", num);
+
+    // constant in c programming
+
+    // const int numb = 10;
+    // printf("%d", numb);
+    // numb = 20;
+    // printf("%d", numb);
+
+    printf("hello world\n");
+    printf("%d\n",40);
+
+    // ***** user input in c **** //
+    // int age;
+    // printf("Enter your age\n");
+    // scanf("%d", &age);
+    // printf("you are %d years old", age);
+
+    // char name;
+    // printf("question 1: what is your name\n");
+    // scanf("%s", &name);
+    // printf("my name is %s i'm 300 years old");
+
+    // double cgpa;
+    // printf("what is your cgpa?\n");
+    // scanf("%lf", &cgpa);
+    // printf("my cpga is %lf", cgpa);
+
+    // int num1;
+    // int num2;
+    // printf("Enter your first number\n");
+    // scanf("%d", &num1);
+
+    // printf("Enter your second number\n");
+    // scanf("%d", &num2);
+
+    // printf("%d", num1 + num2);
+
+    // int multi1;
+    // int multi2;
+
+    // printf("Enter your first number\n");
+    // scanf("%d", &multi1);
+
+    // printf("Enter your second number\n");
+    // scanf("%d", &multi2);
+
+    // printf("%d", multi1 * multi2);
+
+    char color[20];
+    char pluralNoun[20];
+    char celebrity[20];
+    char name[20];
+
+    printf("Enter a color\n");
+    scanf("%s",color);
+    printf("Enter a plural noun\n");
+    scanf("%s", pluralNoun);
+    printf("Enter a celebrity\n");
+    scanf("%s",celebrity);
+    printf("Enter a name");
+    scanf("%s", name);
+
+    
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", pluralNoun);
+    printf("i love \n", celebrity);
+    printf("my name is \n", name);
+
+    return 0;
+
+}
