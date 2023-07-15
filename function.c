75d08b54ed246afef837c402bfbc4b5eb7bb1fd3
Alayande olaoluwa Stephen <alayandesteven@gmail.com>
2023-07-15 12:32:13 +0100

    function practice in c

--- function.c
+++ function.c
@@ -2,5 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-    
+
+    sayHi("olaoluwa");
+    sayHi("odel");
+    sayHi("kunle");
+    return 0;
+}
+
+void sayHi(char name[]) {
+    printf("hello %s\n", name);
 }
