3e6b03f5e0b9c2f58c86b5d018942fee534c5fa6
Alayande olaoluwa Stephen <alayandesteven@gmail.com>
2023-07-15 16:09:24 +0100

    if statement in c

--- condition.c
+++ condition.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main () {
+int max(int num1, int num2) {
+    int result;
     
+    if(num1 > num2) {
+        result = num1;
+
+    }else {
+        result = num2;
+    }
+
+
+}
+
+int main () {
+    printf("%d",max(10,20));
 
     return 0;
 }
