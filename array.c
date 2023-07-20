2231faa63d29a87c2e060d9536afdc0e4ad840a1
Alayande olaoluwa Stephen <alayandesteven@gmail.com>
2023-07-20 18:18:34 +0100

    c master

--- array.c
+++ array.c
@@ -9,41 +9,64 @@
 
     };
 
-int main() {
-    int luckyNumbers[] = {2,14,13,24,16,28};
-    int numbers[5];
-    char name[] = "array of character";
-    numbers[0] = 1000;
-    luckyNumbers[3] = 500;
-    printf("%d\n", luckyNumbers[1]);
-    printf("%d\n", luckyNumbers[3]);
-    printf("%d\n", numbers[0]);
-    printf("%s\n", name);
+// int main() {
+//     int luckyNumbers[] = {2,14,13,24,16,28};
+//     int numbers[5];
+//     char name[] = "array of character";
+//     numbers[0] = 1000;
+//     luckyNumbers[3] = 500;
+//     printf("%d\n", luckyNumbers[1]);
+//     printf("%d\n", luckyNumbers[3]);
+//     printf("%d\n", numbers[0]);
+//     printf("%s\n", name);
 
 
-    ////********* STRUCTS IN C *************///
-    // sruct is like a container for storing data of any data type in c programming//
+//     ////********* STRUCTS IN C *************///
+//     // sruct is like a container for storing data of any data type in c programming//
 
-  struct student student1;
-  student1.age = 30;
-  student1.cgpa = 3.8;
-  strcpy(student1.name,"olaoluwa");
-  strcpy(student1.major, "engineering");
+//   struct student student1;
+//   student1.age = 30;
+//   student1.cgpa = 3.8;
+//   strcpy(student1.name,"olaoluwa");
+//   strcpy(student1.major, "engineering");
 
-  struct student student2;
-  student2.age = 20;
-  student2.cgpa = 3.7;
-  strcpy(student2.name, "badmus");
-  strcpy(student2.major, "Accounting");
+//   struct student student2;
+//   student2.age = 20;
+//   student2.cgpa = 3.7;
+//   strcpy(student2.name, "badmus");
+//   strcpy(student2.major, "Accounting");
 
-  printf("%s\n",student1.name);
-  printf("%f\n", student1.cgpa);
-  printf("%d\n", student1.age);
-  printf("%s\n", student1.major);
+//   printf("%s\n",student1.name);
+//   printf("%f\n", student1.cgpa);
+//   printf("%d\n", student1.age);
+//   printf("%s\n", student1.major);
 
-  printf("%s\n", student2.name);
-  printf("%d", student2.age);
+//   printf("%s\n", student2.name);
+//   printf("%d", student2.age);
     
 
-    return 0;
-}
+//     return 0;
+// }
+
+// ************ 2D Arrays ****************//
+
+int main() {
+  int nums[3][2] = {
+    {1, 2},
+    {3, 4},
+    {5, 6}
+  };
+
+  int i, j;
+
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 2; j++) {
+      printf("%d, ", nums[i][j]);
+    }
+    printf("\n");
+  }
+
+  // printf("%d", nums[1] [1]);
+
+  return 0;
+};
---
+++ memory.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() { 
+    int age = 20;
+    int * pAge = &age;
+    double cgpa = 3.6;
+    double * pCgpa = &cgpa;
+    char name[] = "olaoluwa";
+    char * pName = &name;
+    printf("%p", * pCgpa);
+
+    // derefrencing in c programing//
+    printf("d", )
+
+
+
+    printf("age: %p\n cgpa: %p\n name: %p", &age, &cgpa, &name);
+
+    // ****** pointers in c programming ******//
+    
+
+    return 0;
+
+};
+
